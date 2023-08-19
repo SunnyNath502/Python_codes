2ed56004c99b107cbc34660fda7851130390d48f
SunnyNath502 <snath6374@gmail.com>
2023-08-19 23:29:26 +0530

    random commit

--- 2423.py
+++ 2423.py
@@ -1,13 +1,21 @@
+from collections import Counter
 class Solution:
     """We check if deleting a letter from the string 
     will give us a set of frequencies of other letters 
     with len == 1, basically that every other letter
     occurs same number of times
     """
+    # def equalFrequency(self, word: str) -> bool:
+    #     for i, _ in enumerate(word):
+    #         if len(set(Counter((word[:i]+word[i+1:])).values())) == 1:
+    #             return True
+            
+    #     return False
     def equalFrequency(self, word: str) -> bool:
         for i, _ in enumerate(word):
-            if len(set(Counter((word[:i]+word[i+1:])).values())) == 1:
-                return True
-            
-        return False
+        #   print(word[:i]+word[i+1:])  
+          print(len(set(Counter((word[:i]+word[i+1:])).values())))
+
         
+obj=Solution().equalFrequency('abcdc')
+print(obj)
