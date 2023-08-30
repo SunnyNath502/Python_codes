6b0e01a9dcc3c67aecd902188f704a04b43e2b45
SunnyNath502 <snath6374@gmail.com>
2023-08-30 23:40:16 +0530

    first commit

---
+++ 153.py
@@ -0,0 +1,8 @@
+#  Find Minimum in Rotated Sorted Array
+nm=[0,1,2,3]
+n=len(nm)
+k=4
+r=[]
+for i in range(n):
+    r.append(nm[n-1-i])
+print(r)
---
+++ 2236.py
@@ -0,0 +1,2 @@
+nums=[4,7,8,2]
+print(nums==sorted(nums))
